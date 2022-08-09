77ae21a3a15b7aae93ae9048247caa9cc897eea8
Humberto de Oliveira <humbertosilva06@gmail.com>
2022-08-09 16:22:12 -0300

    resolução dos exercicos (sem correção)

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,2 +1,19 @@
-const num = //seu numero aqui
+const num =+ prompt("digite um numero")
+
+//com ifs aninnhados
+// não consegui pensar em como resolver isso com if aninhado, só se for divisivel pelos dois numeros, por um ou outro não consegui
+
+/*if (num % 2 === 0){
+    if(num % 3 === ){
+        
+
+    }
+}*/
+
+// com operador logico
+
+if (num % 2 === 0 || num % 3 === 0){
+    console.log(" o numero é divisivel por 2 ou 3")
+}
+
 
